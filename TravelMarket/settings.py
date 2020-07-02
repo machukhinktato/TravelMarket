170956c1b50723e3cd721d6e47b718040d999075
machukhinktato <tarabrinmv@gmail.com>
2020-07-02 11:53:13 +0300

    authapp added in settings.py like and auth_user_model

--- TravelMarket/settings.py
+++ TravelMarket/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
