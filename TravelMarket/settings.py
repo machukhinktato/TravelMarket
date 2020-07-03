be1c9eeda21c3cc0b1f52daf5d644884c4c15481
machukhinktato <tarabrinmv@gmail.com>
2020-07-03 14:35:46 +0300

    media added,

--- TravelMarket/settings.py
+++ TravelMarket/settings.py
@@ -125,5 +125,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AUTH_USER_MODEL = 'authapp.ShopUser'
+
+# AUTH_USER_MODEL = 'authapp.ShopUser'
