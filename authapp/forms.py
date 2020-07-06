c82db3b3f9a4e20cdb706c91a5d7da959740d911
machukhinktato <tarabrinmv@gmail.com>
2020-07-06 15:30:39 +0300

    edit form and views done

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 
 from .models import ShopUser
 
@@ -27,6 +28,30 @@ class ShopUserRegisterForm(UserCreationForm):
             for field_name, field in self.fields.items():
                 field.widget.attrs['class'] = 'form-control'
                 field.help_texts = ''
+
+        def clean_age(self):
+            data = self.cleaned_data['age']
+            if data < 18:
+                raise forms.ValidationErrors(
+                    'Забронировать размещение возможно лишь с 18-ти лет')
+
+            return data
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = (
+            'username', 'first_name', 'email', 'age', 'avatar', 'password'
+        )
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(self, *args, **kwargs)
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'form-control'
+                field.help_texts = ''
+                if field_name == 'password':
+                    field.widget = forms.HiddenInput()
 
         def clean_age(self):
             data = self.cleaned_data['age']
--- authapp/views.py
+++ authapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
-from authapp.forms import ShopUserLoginForm
-from authapp.forms import ShopUserRegisterForm
 from django.contrib import auth
 from django.urls import reverse
+from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserRegisterForm
+from authapp.forms import ShopUserEditForm
 
 
 def login(request):
@@ -38,11 +39,11 @@ def register(request):
 
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        print(type(register_form))
 
         if register_form.is_valid():
             register_form.save()
-            # cleaned_form = register_form.cleaned_data
-            # cleaned_form.update()
+            return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
 
@@ -51,4 +52,17 @@ def register(request):
     return render(request, 'authapp/register.html', content)
 
 def edit(request):
-    return HttpResponseRedirect(reverse('main'))
+
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    content = {'title': title, 'edit_form': edit_form}
+
+    return render(request, 'authapp/edit.html', content)
