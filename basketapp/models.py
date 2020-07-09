4b7c6a3702ed82d1f9d984431cc7e89c0e16a61e
machukhinktato <tarabrinmv@gmail.com>
2020-07-09 14:28:23 +0300

    models of basketapp added

---
+++ basketapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from mainapp.models import Accommodation
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='basket'
+    )
+
+    accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
+    staying = models.PositiveIntegerField(verbose_name='сутки', default=0)
+    add_datatime = models.DateTimeField(verbose_name='время', auto_now_add=True)
