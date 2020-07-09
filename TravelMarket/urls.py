c52b71f0fb82fd22d09ef7e47e8640ad0b9d10e3
machukhinktato <tarabrinmv@gmail.com>
2020-07-09 14:22:34 +0300

    templates of mainapp fixed

--- TravelMarket/urls.py
+++ TravelMarket/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', mainapp.main, name='main'),
     path('list_of_accommodations/', include('mainapp.urls', namespace='acc')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='baskety')),
 ]
 
 if settings.DEBUG:
