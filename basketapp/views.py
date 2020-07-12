d173496e570bd52516935befb38c5dcb7eed14bb
machukhinktato <tarabrinmv@gmail.com>
2020-07-12 19:00:33 +0300

    basket may add accommodation

--- basketapp/views.py
+++ basketapp/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from basketapp.models import Basket
-from mainapp.models import Product
+from mainapp.models import Accommodation
 
 
-def Basket(request):
+def basket(request):
     content = {}
     return render(request, 'basketapp/basket.html', content)
 
 
 def basket_add(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    basket = Basket.objects.filter(user=request.user, product=product)
+    accommodation = get_object_or_404(Accommodation, pk=pk)
+    basket = Basket.objects.filter(user=request.user, accommodation=accommodation).first()
+
+    if not basket:
+        basket = Basket(user=request.user, accommodation=accommodation)
 
     basket.staying += 1
     basket.save()
