ac79720cd7f41264347283b6d81276a9186fa1a1
machukhinktato <tarabrinmv@gmail.com>
2020-07-17 14:19:51 +0300

    admin forms filled to work with countries db

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 from authapp.models import ShopUser
 from authapp.forms import ShopUserEditForm
+from mainapp.models import ListOfCountries
 
 
 class ShopUserAdminEditForm(ShopUserEditForm):
     class Meta:
         model = ShopUser
         fields = '__all__'
+
+
+class ListOfCountriesEditForm(forms.ModelForm):
+    class Meta:
+        model = ListOfCountries
+        fields = '__all__'
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
