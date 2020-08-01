34f3f28562b6d1b118ae643559179b96486f01c1
machukhinktato <tarabrinmv@gmail.com>
2020-08-01 15:16:30 +0300

    slightly modified accommodation class in models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -23,7 +23,7 @@ class Regions(models.Model):
 class Accommodation(models.Model):
     country = models.ForeignKey(ListOfCountries, on_delete=models.CASCADE)
     region = models.ForeignKey(Regions, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='название проживания', max_length=128)
+    name = models.CharField(verbose_name='название проживания', max_length=128, unique=True)
     image = models.ImageField(upload_to='accommodation_img', blank=True)
     short_desc = models.TextField(verbose_name='краткое описание продукта', max_length=60, blank=True)
     room_desc = models.TextField(verbose_name='краткое описание комнаты', max_length=60, blank=True)
@@ -33,10 +33,27 @@ class Accommodation(models.Model):
     availability = models.PositiveIntegerField(verbose_name='количество свободных номеров')
     is_active = models.BooleanField(verbose_name='активна', default=True)
 
-
     @staticmethod
     def get_items():
-        return Accommodation.objects.filter(is_active=True).order_by('country','regions', 'name')
+        return Accommodation.objects.filter(is_active=True).order_by('country', 'regions', 'name')
 
     def __str__(self):
         return f'{self.name} ({self.country.name})'
+
+
+# class Bookings(models.Model):
+#     hotel = models.ForeignKey(Accommodation, on_delete=models.CASCADE, default='')
+#     date = models.DateField()  # date of booking
+#     # room = models.ForeignKey(Room, on_delete=models.CASCADE)  # room which we are trying to book
+#     client_name = models.CharField(max_length=100)
+#     client_email = models.CharField(max_length=100)  # client's email
+#     phone_number = models.CharField(max_length=20,  verbose_name="Client's phone number")
+#     time = models.CharField(max_length=50, choices=arrival_time,
+#                             default='12:00')  # approximate time of check in
+#     comments = models.CharField(max_length=500)  # client's requests
+#     country = models.CharField(max_length=50, choices=country_dict,
+#                                default='Russia')
+#     address = models.CharField(max_length=100)  # client's address of living
+#
+#     def __str__(self):
+#         return 'Room Booking {} - {}'.format(self.room.name, self.room.hotel.name)
